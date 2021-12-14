eea13af91d58fccbe0f3c9e10cc91407460fd20d
sumon6638 <mdsumoncse19@gmail.com>
2021-12-15 03:22:01 +0600

    navOk

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,25 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  theme: {
+    screens: {
+      'xs': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      'sm': '480px',
+      // => @media (min-width: 480px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1440px) { ... }
+
+      '2xl': '1280px',
+      // => @media (min-width: 2560px) { ... }
+    }
+  }
 }
